d17edd568f2501d6ab52d033e37b5b98b46c94b2
nobodydi <149184961+nobodydi@users.noreply.github.com>
2024-01-14 19:57:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+#OWNER_ID = int(getenv("6358262816"))
+OWNER_ID = int(getenv("OWNER_ID", "6358262816"))
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+MUST_JOIN = getenv("MUST_JOIN", None)
